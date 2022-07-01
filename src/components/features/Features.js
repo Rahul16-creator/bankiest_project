2cc5bdb755379f18eee012cc7ac2b6fe3d65f92c
Rahul krishnan <rahul.r5@myntra.com>
2022-07-01 20:56:23 +0530

    changes

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -4,8 +4,37 @@ import "./features.css";
 const Features = ({ currentRef }) => {
   return (
     <div ref={currentRef} className="section section-features">
-      <div>Heading</div>
-      <div>grids</div>
+      <div className="feature_heading">
+        <p>FEATURES</p>
+        <h1>Everything you need in a modern bank and more.</h1>
+      </div>
+      <div className="feature_content">
+        <img src="/assets/digital.jpg" alt="Digital img" />
+        <div className="feature_content_features">
+          <div className="features_icon">
+            <ion-icon name="tv-outline"></ion-icon>
+          </div>
+          <h2>100% digital bank</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde alias sint quos? Accusantium a fugiat porro reiciendis saepe quibusdam debitis ducimus</p>
+        </div>
+        <div className="feature_content_features">
+          <div className="features_icon">
+            <ion-icon name="tv-outline"></ion-icon>
+          </div>
+          <h2>Watch your money grow</h2>
+          <p>Nesciunt quos autem dolorum voluptates cum dolores dicta fuga inventore ab? Nulla incidunt eius numquam sequi iste pariatur quibusdam</p>
+        </div>
+        <img src="/assets/card.jpg" alt="Card img" />
+        <img src="/assets/grow.jpg" alt="Grow img" />
+
+        <div className="feature_content_features">
+          <div className="features_icon">
+            <ion-icon name="server-outline"></ion-icon>
+          </div>
+          <h2>Free debit card included</h2>
+          <p>Quasi, fugit in cumque cupiditate reprehenderit debitis animi enim eveniet consequatur odit quam quos possimus assumenda dicta fuga inventore ab.</p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/views/page.jsx
+++ src/views/page.jsx
@@ -1,20 +1,20 @@
 import React, { useRef } from "react";
-import Features from "../components/features/Features";
 import Header from "../components/headers/Header";
 import ErrorBoundary from "../utils/ErrorBoundary";
 
 const Home = React.lazy(() => import("../components/home/Home"));
+const Features = React.lazy(() => import("../components/features/Features"));
 
 const HeaderPage = () => {
   const currentRef = useRef(null);
   return (
     <React.Fragment>
-      <Header currentRef={currentRef} />
       <ErrorBoundary>
         <React.Suspense fallback="Loading...">
+          <Header currentRef={currentRef} />
           <Home />
+          <Features currentRef={currentRef} />
         </React.Suspense>
-        <Features currentRef={currentRef} />
       </ErrorBoundary>
     </React.Fragment>
   );
